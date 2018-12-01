5c1c7ecbad89128c1afeee3365dde4b820f4543b
Abhinav <abhinav95@hotmail.com>
2018-12-02 03:41:25 +0530

    factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,8 +1,11 @@
-
-function factorial(...args) {
-  return args;
+function factorial(args) {
+  let num = 1;
+  let counter = args;
+  while (counter > 1) {
+    num *= counter;
+    counter -= 1;
+  }
+  return num;
 }
 
-export {
-  factorial,
-};
+export { factorial };

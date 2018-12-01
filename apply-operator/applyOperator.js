d1ba26c6bfb7bc8a72a359ee2c6053048b0fac9d
Abhinav <abhinav95@hotmail.com>
2018-12-02 02:08:07 +0530

    Apply Operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,39 +1,44 @@
 
-function applyOperator() {
+function applyOperator(...args) {
+  const index = 1;
+  const operatorIndex = 0;
+  if (!args[operatorIndex]) {
+    throw new Error();
+  }
+  if (!args[index]) { return 0; }
 
-  if(arguments.length <= 1)
-  return 0;
-  
-  if(arguments.length === 2)
-  return arguments[1];
-
-  var result = 0;
-  switch (arguments[0]){
+  // if (args.length === 2) { return args[1]; }
+  let result;
+  switch (args[0]) {
     case '+':
-      for(var i = 1; i<arguments.length;i++)
-        result = result + arguments[i];
-      break;
+      result = 0;
+      for (let i = 1; i < args.length; i += 1) result += args[i];
+      return result;
 
     case '-':
-      result = arguments[1];
-      for(i=2;i<arguments.length;i++)
-        result = result - arguments[i];
-      break;
+      result = 0;
+      for (let i = 1; i < args.length; i += 1) result -= args[i];
+      return result;
+
 
     case '*':
       result = 1;
-      for(i=1;i<arguments.length;i++)
-        result = result * arguments[i];
-      break;
+      for (let i = 1; i < args.length; i += 1) result *= args[i];
+      return result;
     case '/':
-    result = arguments[1];
-    for(i=2;i<arguments.length;i++)
-      result = result / arguments[i];
-    break;
+      result = args[index];
+      for (let i = 2; i < args.length; i += 1) result /= args[i];
+      result = Math.round(result * 10000) / 10000;
+      return result;
+    case '%':
+      result = args[index];
+      for (let i = 2; i < args.length; i += 1) result %= args[i];
+      return result;
+    default:
+      return result;
   }
-
-  return result;
 }
+
 
 export {
   applyOperator,

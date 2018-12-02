163be63ba484c051ff86926d1cd842bf1fad9576
Abhinav <abhinav95@hotmail.com>
2018-12-02 06:39:46 +0530

    In Progress Rotated string

--- rotated-string/rotatedString.js
+++ rotated-string/rotatedString.js
@@ -1,8 +1,21 @@
-
-function rotatedString(...args) {
-  return args;
+function rotate(arg1) {
+  // eslint-disable-next-line prefer-const
+  let arg = arg1;
+  const len = arg.length;
+  const lelement = arg[len - 1];
+  for (let i = 0; i < len - 1; i += 1) {
+    arg[i + 1] = arg[i];
+  }
+  arg[0] = lelement;
+  return arg;
+}
+function rotatedString(arg, arg3) {
+  if (arg === arg3) return false;
+  let flag = false;
+  for (let i = 0; i < arg.length; i += 1) {
+    if (rotate(arg) === arg3) flag = true;
+  }
+  return flag;
 }
 
-export {
-  rotatedString,
-};
+export { rotatedString };
